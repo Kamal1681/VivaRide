14fc55817ab24fc27a22bb969dda5f86d59ced09
Pavel Uvarov <p.uvarov@gmail.com>
2019-02-26 11:45:38 -0500

    Initial Commit

---
+++ W8D2-Ride-Share/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  W8D2-Ride-Share
+//
+//  Created by Pavel on 2019-02-26.
+//  Copyright © 2019 Pavel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
