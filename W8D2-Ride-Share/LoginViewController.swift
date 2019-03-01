04ffb20c250d03c79963285d9cbee157a93c97b3
Pavel Uvarov <p.uvarov@gmail.com>
2019-03-01 18:44:44 -0500

    Login View Controller working with sessions. Can Sign out. Next step - create a registration.

--- W8D2-Ride-Share/LoginViewController.swift
+++ W8D2-Ride-Share/LoginViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Firebase
 
 class LoginViewController: UIViewController {
 
@@ -14,13 +15,65 @@ class LoginViewController: UIViewController {
     @IBOutlet weak var signUpButton: UIButton!
     @IBOutlet weak var fogotPassButton: UIButton!
     
+    @IBOutlet weak var emailField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    
     @IBAction func signInButton(_ sender: UIButton) {
+        
+        guard let email = self.emailField.text, let password = self.passwordField.text else {
+            print("Error")
+            return
+        }
+        
+        if email == "" || password == "" {
+            let alert = UIAlertController(title: "Alert", message: "Email or Password can not be empty!", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+            self.present(alert, animated: true, completion: nil)
+        }
+        
+
+            // [START headless_email_auth]
+        Auth.auth().signIn(withEmail: email, password: password) { (authDataResult, errorCurrent) in
+                if authDataResult != nil {
+                    self.dismiss(animated: true, completion: nil)
+                }
+                else {
+                    let alert = UIAlertController(title: "Alert", message: "Email and/or Password is incorrect!", preferredStyle: .alert)
+                    alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+                    self.present(alert, animated: true, completion: nil)
+                    print(authDataResult ?? "No authDataResult")
+                }
+            
+        }
+        
+//            Auth.auth().signIn(withEmail: email, password: password) { [weak self] user, error in
+//                guard let strongSelf = self else { return }
+//                // [START_EXCLUDE]
+//                strongSelf.hideSpinner {
+//                    if let error = error {
+//                        strongSelf.showMessagePrompt(error.localizedDescription)
+//                        return
+//                    }
+//                    strongSelf.navigationController?.popViewController(animated: true)
+//                }
+//                // [END_EXCLUDE]
+//            }
+//            // [END headless_email_auth]
+
+        
+//        self.dismiss(animated: true, completion: nil)
+
+
     }
     
     @IBAction func signUpButton(_ sender: UIButton) {
